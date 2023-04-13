24a0a9d282202b7f05ba819daa4f7afba92574ca
barilo <e.barilo@isida.by>
2023-04-13 16:56:54 +0300

    fix сохранения isRankDiscountOn

--- src/main/java/tgb/btc/rce/service/processors/ChangeRankDiscountProcessor.java
+++ src/main/java/tgb/btc/rce/service/processors/ChangeRankDiscountProcessor.java
@@ -3,12 +3,15 @@ package tgb.btc.rce.service.processors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import tgb.btc.rce.annotation.CommandProcessor;
+import tgb.btc.rce.bean.User;
+import tgb.btc.rce.bean.UserDiscount;
 import tgb.btc.rce.constants.BotStringConstants;
 import tgb.btc.rce.enums.Command;
 import tgb.btc.rce.repository.UserDiscountRepository;
 import tgb.btc.rce.repository.UserRepository;
 import tgb.btc.rce.service.IResponseSender;
 import tgb.btc.rce.service.Processor;
+import tgb.btc.rce.service.impl.UserDiscountService;
 import tgb.btc.rce.service.impl.UserService;
 import tgb.btc.rce.util.UpdateUtil;
 
@@ -18,6 +21,20 @@ public class ChangeRankDiscountProcessor extends Processor {
     private RankDiscountProcessor rankDiscountProcessor;
 
     private UserDiscountRepository userDiscountRepository;
+
+    private UserDiscountService userDiscountService;
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Autowired
+    public void setUserDiscountService(UserDiscountService userDiscountService) {
+        this.userDiscountService = userDiscountService;
+    }
 
     @Autowired
     public void setUserDiscountRepository(UserDiscountRepository userDiscountRepository) {
@@ -39,7 +56,15 @@ public class ChangeRankDiscountProcessor extends Processor {
         String[] values = update.getCallbackQuery().getData().split(BotStringConstants.CALLBACK_DATA_SPLITTER);
         Long userChatId = Long.valueOf(values[1]);
         Boolean isRankDiscountOn = Boolean.valueOf(values[2]);
-        userDiscountRepository.updateIsRankDiscountOnByChatId(isRankDiscountOn, userChatId);
+        Long userPid = userRepository.getPidByChatId(userChatId);
+        if (userDiscountService.isExistByUserPid(userPid)) {
+            userDiscountRepository.updateIsRankDiscountOnByChatId(isRankDiscountOn, userChatId);
+        } else {
+            UserDiscount userDiscount = new UserDiscount();
+            userDiscount.setUser(new User(userPid));
+            userDiscount.setRankDiscountOn(isRankDiscountOn);
+            userDiscountRepository.save(userDiscount);
+        }
         responseSender.deleteMessage(UpdateUtil.getChatId(update), update.getCallbackQuery().getMessage().getMessageId());
         rankDiscountProcessor.sendUserRankDiscount(UpdateUtil.getChatId(update), userChatId);
     }
