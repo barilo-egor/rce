60ed7a1d87413707f0ffd6a6fdf16f34e5f35241
hodikov <hodikov@isida.by>
2023-06-12 10:28:31 +0300

    help command

--- src/main/java/tgb/btc/rce/enums/Command.java
+++ src/main/java/tgb/btc/rce/enums/Command.java
@@ -11,171 +11,174 @@ public enum Command {
      * CallbackQuery
      */
 
-    START("/start", false, false),
-    NONE("none", false, true),
+    START("/start", false, false, false),
+    NONE("none", false, true, false),
 
     /*
       Reply
      */
 
     /** UTIL */
-    BACK("◀️ Назад", false, true),
-    ADMIN_BACK("Назад", true, false),
-    CANCEL("Отмена", false, false),
-    SHARE_CONTACT("Поделиться контактом", false, false),
-    BOT_OFFED("bot_offed", false, true),
-    INLINE_DELETE("inline_delete", false, true),
-    CAPTCHA("captcha", false, false),
+    BACK("◀️ Назад", false, true, false),
+    ADMIN_BACK("Назад", true, false, false),
+    CANCEL("Отмена", false, false, false),
+    SHARE_CONTACT("Поделиться контактом", false, false, false),
+    BOT_OFFED("bot_offed", false, true, false),
+    INLINE_DELETE("inline_delete", false, true, false),
+    CAPTCHA("captcha", false, false, false),
 
     /**
      * HIDDEN
      */
-    DELETE_USER("/deleteuser", true, false),
-    CREATE_USER_DATA("/createuserdata", true, false),
-    MAKE_ADMIN("/makeadmin", true, false),
+    DELETE_USER("/deleteuser", true, false, true),
+    CREATE_USER_DATA("/createuserdata", true, false, true),
+    MAKE_ADMIN("/makeadmin", true, false, true),
+    HELP("/help", true, false, true),
 
     /** MAIN */
-    BUY_BITCOIN("\uD83D\uDCB0 Купить", false, false),
-    SELL_BITCOIN("\uD83D\uDCC8 Продать", false, false),
-    CONTACTS("\uD83D\uDEC3 Контакты", false, true),
-    DRAWS("\uD83C\uDFB0 Розыгрыши", false, true),
-    REFERRAL("\uD83E\uDD1D Реферальная программа", false, false),
-    ADMIN_PANEL("Админ панель", true, true),
+    BUY_BITCOIN("\uD83D\uDCB0 Купить", false, false, false),
+    SELL_BITCOIN("\uD83D\uDCC8 Продать", false, false, false),
+    CONTACTS("\uD83D\uDEC3 Контакты", false, true, false),
+    DRAWS("\uD83C\uDFB0 Розыгрыши", false, true, false),
+    REFERRAL("\uD83E\uDD1D Реферальная программа", false, false, false),
+    ADMIN_PANEL("Админ панель", true, true, false),
 
     /** DRAWS */
-    LOTTERY("\uD83C\uDFB0 Лотерея", false, false),
-    ROULETTE("\uD83C\uDFB0 Рулетка", false, true),
+    LOTTERY("\uD83C\uDFB0 Лотерея", false, false, false),
+    ROULETTE("\uD83C\uDFB0 Рулетка", false, true, false),
 
     /** REFERRAL */
-    WITHDRAWAL_OF_FUNDS("withdrawal", false, false),
-    SHOW_WITHDRAWAL_REQUEST("show_withdrawal", false, false),
-    SEND_LINK(BotProperties.BOT_CONFIG_PROPERTIES.getString("bot.link"), false, false),
-    HIDE_WITHDRAWAL("hide_withdrawal", true, false),
-    CHANGE_REFERRAL_BALANCE("change_ref", true, false),
-    DELETE_WITHDRAWAL_REQUEST("withdrawal_delete", false, false),
+    WITHDRAWAL_OF_FUNDS("withdrawal", false, false, false),
+    SHOW_WITHDRAWAL_REQUEST("show_withdrawal", false, false, false),
+    SEND_LINK(BotProperties.BOT_CONFIG_PROPERTIES.getString("bot.link"), false, false, false),
+    HIDE_WITHDRAWAL("hide_withdrawal", true, false, false),
+    CHANGE_REFERRAL_BALANCE("change_ref", true, false, false),
+    DELETE_WITHDRAWAL_REQUEST("withdrawal_delete", false, false, false),
 
     /** ADMIN PANEL */
-    REQUESTS("Заявки", true, true),
-    SEND_MESSAGES("Отправка сообщений", true, true),
-    BAN_UNBAN("Бан/разбан", true, false),
-    BOT_SETTINGS("Настройки бота", true, true),
-    REPORTS("Отчеты", true, true),
-    EDIT_CONTACTS("Редактирование контактов", true, true),
-    USER_REFERRAL_BALANCE("Реф.баланс юзера", true, false),
-    CHANGE_USD_COURSE("Курс доллара", true, false),
-    TURNING_CURRENCY("Включение криптовалют", true, false),
-    DISCOUNTS("Скидки", true, true),
-    USERS("Пользователи", true, true),
-    QUIT_ADMIN_PANEL("Выйти", true, false),
+    REQUESTS("Заявки", true, true, false),
+    SEND_MESSAGES("Отправка сообщений", true, true, false),
+    BAN_UNBAN("Бан/разбан", true, false, false),
+    BOT_SETTINGS("Настройки бота", true, true, false),
+    REPORTS("Отчеты", true, true, false),
+    EDIT_CONTACTS("Редактирование контактов", true, true, false),
+    USER_REFERRAL_BALANCE("Реф.баланс юзера", true, false, false),
+    CHANGE_USD_COURSE("Курс доллара", true, false, false),
+    TURNING_CURRENCY("Включение криптовалют", true, false, false),
+    DISCOUNTS("Скидки", true, true, false),
+    USERS("Пользователи", true, true, false),
+    QUIT_ADMIN_PANEL("Выйти", true, false, false),
 
     /** DISCOUNTS */
-    RANK_DISCOUNT("Ранговая скидка(персональная)", true, false),
-    CHANGE_RANK_DISCOUNT("change_rank_discount", true, false),
-    PERSONAL_BUY_DISCOUNT("Персональная, покупка", true, false),
-    PERSONAL_SELL_DISCOUNT("Персональная, продажа", true, false),
-    BULK_DISCOUNTS("Оптовые скидки", true, false),
-    REFERRAL_PERCENT("Процент реферала", true, false),
-    TURN_RANK_DISCOUNT("Ранговая скидка(для всех)", true, false),
-    TURNING_RANK_DISCOUNT("turning_rd", true, false),
+    RANK_DISCOUNT("Ранговая скидка(персональная)", true, false, false),
+    CHANGE_RANK_DISCOUNT("change_rank_discount", true, false, false),
+    PERSONAL_BUY_DISCOUNT("Персональная, покупка", true, false, false),
+    PERSONAL_SELL_DISCOUNT("Персональная, продажа", true, false, false),
+    BULK_DISCOUNTS("Оптовые скидки", true, false, false),
+    REFERRAL_PERCENT("Процент реферала", true, false, false),
+    TURN_RANK_DISCOUNT("Ранговая скидка(для всех)", true, false, false),
+    TURNING_RANK_DISCOUNT("turning_rd", true, false, false),
 
     /** TURNING CURRENCIES */
-    TURN_ON_CURRENCY("turn_on_currency", true, false),
-    TURN_OFF_CURRENCY("turn_off_currency", true, false),
+    TURN_ON_CURRENCY("turn_on_currency", true, false, false),
+    TURN_OFF_CURRENCY("turn_off_currency", true, false, false),
 
     /** EDIT CONTACTS */
-    ADD_CONTACT("Добавить контакт", true, false),
-    DELETE_CONTACT("Удалить контакт", true, false),
+    ADD_CONTACT("Добавить контакт", true, false, false),
+    DELETE_CONTACT("Удалить контакт", true, false, false),
 
     /** SEND MESSAGES */
-    MAILING_LIST("Рассылка", true, false),
-    SEND_MESSAGE_TO_USER("Сообщение пользователю", true, false),
+    MAILING_LIST("Рассылка", true, false, false),
+    SEND_MESSAGE_TO_USER("Сообщение пользователю", true, false, false),
 
     /** BOT SETTINGS */
-    CURRENT_DATA("Текущие данные", true, false),
-    ON_BOT("Вкл.бота", true, false),
-    OFF_BOT("Выкл.бота", true, false),
-    BOT_MESSAGES("Сообщения бота", true, false),
-    BOT_VARIABLES("Переменные бота", true, false),
-    SYSTEM_MESSAGES("Сис.сообщения", true, false),
-    PAYMENT_TYPES("Типы оплаты", true, false),
+    CURRENT_DATA("Текущие данные", true, false, false),
+    ON_BOT("Вкл.бота", true, false, false),
+    OFF_BOT("Выкл.бота", true, false, false),
+    BOT_MESSAGES("Сообщения бота", true, false, false),
+    BOT_VARIABLES("Переменные бота", true, false, false),
+    SYSTEM_MESSAGES("Сис.сообщения", true, false, false),
+    PAYMENT_TYPES("Типы оплаты", true, false, false),
 
     /** DEAL */
-    DEAL("deal_proc", false, false),
-    PAID("Я оплатил(а)", false, false),
-    CANCEL_DEAL("Отменить заявку", false, false),
-    DELETE_DEAL("Удалить заявку", false, false),
-    SHOW_DEAL("Показать", true, false),
-    DELETE_USER_DEAL("delete_deal", true, false),
-    DELETE_DEAL_AND_BLOCK_USER("deleteDeal_and_block_user", true, false),
-    CONFIRM_USER_DEAL("confirm_deal", true, false),
-    ADDITIONAL_VERIFICATION("add_verification", true, false),
-    USER_ADDITIONAL_VERIFICATION("user_verification", false, false),
-    SHARE_REVIEW("share_review", false, false),
-    CHOOSING_FIAT_CURRENCY("chs_fc", false, false),
+    DEAL("deal_proc", false, false, false),
+    PAID("Я оплатил(а)", false, false, false),
+    CANCEL_DEAL("Отменить заявку", false, false, false),
+    DELETE_DEAL("Удалить заявку", false, false, false),
+    SHOW_DEAL("Показать", true, false, false),
+    DELETE_USER_DEAL("delete_deal", true, false, false),
+    DELETE_DEAL_AND_BLOCK_USER("deleteDeal_and_block_user", true, false, false),
+    CONFIRM_USER_DEAL("confirm_deal", true, false, false),
+    ADDITIONAL_VERIFICATION("add_verification", true, false, false),
+    USER_ADDITIONAL_VERIFICATION("user_verification", false, false, false),
+    SHARE_REVIEW("share_review", false, false, false),
+    CHOOSING_FIAT_CURRENCY("chs_fc", false, false, false),
 
     /** REQUESTS */
-    NEW_DEALS("Новые заявки", true, false),
-    NEW_WITHDRAWALS("Вывод средств", true, false),
-    NEW_REVIEWS("Новые отзывы", true, false),
+    NEW_DEALS("Новые заявки", true, false, false),
+    NEW_WITHDRAWALS("Вывод средств", true, false, false),
+    NEW_REVIEWS("Новые отзывы", true, false, false),
 
-    PUBLISH_REVIEW("pub_review", true, false),
-    DELETE_REVIEW("del_review", true, false),
+    PUBLISH_REVIEW("pub_review", true, false, false),
+    DELETE_REVIEW("del_review", true, false, false),
 
     /** REPORTS */
-    USERS_REPORT("Отчет по пользователям", true, false),
-    USER_INFORMATION("Информация о пользователе", true, false),
-    USERS_DEALS_REPORT("Отчет сделки пользователей", true, false),
-    DEAL_REPORTS("Отчет по сделкам", true, false),
-    PARTNERS_REPORT("Отчет по партнерам", true, false),
-    CHECKS_FOR_DATE("Чеки по дате", true, false),
-    SEND_CHECKS_FOR_DATE("send_checks_for_dats", true, false),
-    LOTTERY_REPORT("Отчет по лотереи", true, false),
+    USERS_REPORT("Отчет по пользователям", true, false, false),
+    USER_INFORMATION("Информация о пользователе", true, false, false),
+    USERS_DEALS_REPORT("Отчет сделки пользователей", true, false, false),
+    DEAL_REPORTS("Отчет по сделкам", true, false, false),
+    PARTNERS_REPORT("Отчет по партнерам", true, false, false),
+    CHECKS_FOR_DATE("Чеки по дате", true, false, false),
+    SEND_CHECKS_FOR_DATE("send_checks_for_dats", true, false, false),
+    LOTTERY_REPORT("Отчет по лотереи", true, false, false),
 
-    CHANNEL_POST("channel_post", false, false),
+    CHANNEL_POST("channel_post", false, false, false),
 
     /** RECEIPTS */
-    CONTINUE("Продолжить", true, false),
-    RECEIPTS_CANCEL_DEAL("Отменить сделку", true, false),
+    CONTINUE("Продолжить", true, false, false),
+    RECEIPTS_CANCEL_DEAL("Отменить сделку", true, false, false),
 
     /**
      * PAYMENT TYPES
      */
-    NEW_PAYMENT_TYPE("Создать тип оплаты", true, false),
-    DELETE_PAYMENT_TYPE("Удалить тип оплаты", true, false),
-    DELETING_PAYMENT_TYPE("deleting_pt", true, false),
-    NEW_PAYMENT_TYPE_REQUISITE("Создать реквизит", true, false),
-    DELETE_PAYMENT_TYPE_REQUISITE("Удалить реквизит", true, false),
-    DELETING_PAYMENT_TYPE_REQUISITE("delete_ptr", true, false),
-    TURN_PAYMENT_TYPES("Включение типов оплат", true, false),
-    TURNING_PAYMENT_TYPES("turning_pt", true, false),
-    CHANGE_MIN_SUM("Мин.сумма", true, false),
-    TURN_DYNAMIC_REQUISITES("Динамические реквизиты", true, false),
-    TURNING_DYNAMIC_REQUISITES("turning_dr", true, false),
+    NEW_PAYMENT_TYPE("Создать тип оплаты", true, false, false),
+    DELETE_PAYMENT_TYPE("Удалить тип оплаты", true, false, false),
+    DELETING_PAYMENT_TYPE("deleting_pt", true, false, false),
+    NEW_PAYMENT_TYPE_REQUISITE("Создать реквизит", true, false, false),
+    DELETE_PAYMENT_TYPE_REQUISITE("Удалить реквизит", true, false, false),
+    DELETING_PAYMENT_TYPE_REQUISITE("delete_ptr", true, false, false),
+    TURN_PAYMENT_TYPES("Включение типов оплат", true, false, false),
+    TURNING_PAYMENT_TYPES("turning_pt", true, false, false),
+    CHANGE_MIN_SUM("Мин.сумма", true, false, false),
+    TURN_DYNAMIC_REQUISITES("Динамические реквизиты", true, false, false),
+    TURNING_DYNAMIC_REQUISITES("turning_dr", true, false, false),
 
     /**
      * ANTISPAM
      */
-    SHOW_SPAM_BANNED_USER("show_sb_user", true, false),
-    KEEP_SPAM_BAN("keep_sb", true, false),
-    SPAM_UNBAN("spam_unban", true, false),
-    NEW_SPAM_BANS("Антиспам блоки", true, false),
+    SHOW_SPAM_BANNED_USER("show_sb_user", true, false, false),
+    KEEP_SPAM_BAN("keep_sb", true, false, false),
+    SPAM_UNBAN("spam_unban", true, false, false),
+    NEW_SPAM_BANS("Антиспам блоки", true, false, false),
 
     /**
      * USERS STATES
      */
-    NONE_CALCULATOR("none_calc", false, false),
-    INLINE_QUERY_CALCULATOR("inline_q_calc", false, false),
-    INLINE_CALCULATOR("inline_calculator", false, false)
+    NONE_CALCULATOR("none_calc", false, false, false),
+    INLINE_QUERY_CALCULATOR("inline_q_calc", false, false, false),
+    INLINE_CALCULATOR("inline_calculator", false, false, false)
     ;
 
     final String text;
     final boolean isAdmin;
     final boolean isSimple;
+    final boolean isHidden;
 
-    Command(String text, boolean isAdmin, boolean isSimple) {
+    Command(String text, boolean isAdmin, boolean isSimple, boolean isHidden) {
         this.text = text;
         this.isAdmin = isAdmin;
         this.isSimple = isSimple;
+        this.isHidden = isHidden;
     }
 
     public boolean isSimple() {
@@ -188,6 +191,10 @@ public enum Command {
 
     public boolean isAdmin() {
         return isAdmin;
+    }
+
+    public boolean isHidden() {
+        return isHidden;
     }
 
     public static Command fromUpdate(Update update) {
