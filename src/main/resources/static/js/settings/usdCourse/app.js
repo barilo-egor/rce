0eb8ea891163150f06a81e01b74127217afcabe9
barilo <e.barilo@isida.by>
2023-06-15 15:54:00 +0300

    courses

--- src/main/resources/static/js/settings/usdCourse/app.js
+++ src/main/resources/static/js/settings/usdCourse/app.js
@@ -5,5 +5,6 @@ Ext.application({
     name: 'UsdCourse',
     extend: 'Ext.app.Application',
     appFolder: '/js/settings/usdCourse',
+    title: 'Замена курса USD',
     autoCreateViewport: true
 });
--- src/main/resources/static/js/settings/usdCourse/view/UsdCoursePanel.js
+++ src/main/resources/static/js/settings/usdCourse/view/UsdCoursePanel.js
@@ -8,8 +8,6 @@ Ext.define('UsdCourse.view.UsdCoursePanel', {
     },
     bodyStyle: 'padding:5px 5px',
     region: 'center',
-    width: 1000,
-    heigth: 1000,
     layout: {
         type: 'vbox',
         align: 'center'
@@ -18,18 +16,59 @@ Ext.define('UsdCourse.view.UsdCoursePanel', {
         {
             xtype: 'form',
             id: 'coursesForm',
-            width: 300,
-            height: 200,
             listeners: {
-                beforerender: function (me) {
+                afterrender: function (form) {
+                    let me = form;
                     console.log('qwe')
                     Ext.Ajax.request({
                         url: '/settings/getUsdCourses',
                         method: 'GET',
                         success: function (rs) {
-                            let response = Ext.JSON.decode(rs.responseText);
-                            let items = [];
-                            let data = response.data;
+                            let response = Ext.JSON.decode(rs.responseText)
+                            let items = []
+                            let data = response.data
+
+                            for (let fiatCurrency of data) {
+                                let fiatCurrencyItems = []
+                                for (let dealType of fiatCurrency.dealTypes) {
+                                    let dealTypesItems = []
+                                    for (let cryptoCurrency of dealType.cryptoCurrencies) {
+                                        let cryptoCurrencyInput = {
+                                            xtype: 'textfield',
+                                            name: cryptoCurrency.name,
+                                            fieldLabel: cryptoCurrency.name,
+                                            value: cryptoCurrency.value,
+                                            msgTarget: 'side',
+                                            allowBlank: false
+                                        }
+                                        dealTypesItems.push(cryptoCurrencyInput)
+                                    }
+                                    let dealTypeFieldSet = {
+                                        xtype: 'fieldset',
+                                        title: dealType.name,
+                                        collapsible: true,
+                                        layout: 'vbox',
+                                        defaults: {
+                                            labelWidth: 90,
+                                        },
+                                        items: dealTypesItems
+                                    }
+                                    fiatCurrencyItems.push(dealTypeFieldSet)
+                                }
+
+                                let fiatCurrencyFieldSet = {
+                                    xtype: 'fieldset',
+                                    title: fiatCurrency.name,
+                                    collapsible: true,
+                                    defaults: {
+                                        labelWidth: 90,
+                                        anchor: '100%',
+                                        layout: 'hbox'
+                                    },
+                                    items: fiatCurrencyItems
+                                }
+                                me.insert(fiatCurrencyFieldSet)
+                            }
                         }
                     })
                 }
