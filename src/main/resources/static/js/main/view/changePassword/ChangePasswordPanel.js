6a2edadd9fa3aa090406550440ce8035c1e35d82
barilo-egor <EgorBarilo37552109>
2023-08-13 00:22:43 +0300

    change pass refactor

--- src/main/resources/static/js/main/view/changePassword/ChangePasswordPanel.js
+++ src/main/resources/static/js/main/view/changePassword/ChangePasswordPanel.js
@@ -2,115 +2,82 @@ Ext.define('Main.view.changePassword.ChangePasswordPanel', {
     xtype: 'changepasswordpanel',
     extend: 'Main.view.components.FramePanel',
     requires: [
-          'Main.view.components.button.SaveButton'
+        'Main.view.components.button.SaveButton',
+        'Main.view.components.button.EyeButton',
+        'Main.view.changePassword.ChangePasswordController'
     ],
+    controller: 'changePasswordController',
 
+    layout: {
+        type: 'vbox',
+        align : 'stretch'
+    },
     title: {
         xtype: 'mainframetitle',
         text: 'Смена пароля'
     },
-    layout: {
-        type: 'vbox',
-        align : 'center'
-    },
+
     items: [
         {
-            xtype: 'container',
+            xtype: 'panel',
             layout: {
-                type: 'hbox',
-                align: 'center'
+                type: 'vbox',
+                align : 'center'
             },
-            padding: '0 0 10 0',
-            items: [
-                {
+            defaults: {
+                xtype: 'container',
+                layout: {
+                    type: 'hbox',
+                    align: 'center'
+                },
+                padding: '0 0 10 0',
+                defaults: {
                     xtype: 'textfield',
                     labelWidth: 130,
                     labelAlign: 'right',
                     msgTarget: 'side',
-                    id: 'passwordInput',
-                    fieldLabel: 'Новый пароль',
-                    name: 'password',
-                    emptyText: 'Введите пароль',
-                    inputType: 'password',
                     minLength: 8,
-                    validator: ValidatorUtil.validateNotEmptyAndLettersAndNumber
-                },
-                {
-                    xtype: 'button',
-                    iconCls: 'fas fa-eye noColor-noBorderBtn',
-                    cls: 'noColor-noBorderBtn',
-                    handler: function (btn) {
-                        let val = btn.up('container').items.items[0].inputEl.dom.type
-                        if (val === 'password') {
-                            btn.up('container').items.items[0].inputEl.dom.type = 'text'
-                            this.setIconCls('fas fa-eye-slash noColor-noBorderBtn')
-                        } else {
-                            btn.up('container').items.items[0].inputEl.dom.type = 'password'
-                            this.setIconCls('fas fa-eye noColor-noBorderBtn')
-                        }
-                    }
+                    inputType: 'password',
                 }
-            ]
-        },
-        {
-            xtype: 'container',
-            layout: {
-                type: 'hbox',
-                align: 'center'
             },
-            padding: '0 0 10 0',
+            buttonAlign: 'center',
+            buttons: [
+                {
+                    xtype: 'savebutton',
+                    handler: 'saveButtonClick'
+                }
+            ],
             items: [
                 {
-                    xtype: 'textfield',
-                    labelWidth: 130,
-                    labelAlign: 'right',
-                    msgTarget: 'side',
-                    fieldLabel: 'Повторите пароль',
-                    emptyText: 'Введите пароль',
-                    inputType: 'password',
-                    minLength: 8,
-                    validator: ValidatorUtil.validatePasswordConfirm
+                    items: [
+                        {
+
+                            id: 'passwordInput',
+                            fieldLabel: 'Новый пароль',
+                            name: 'password',
+                            emptyText: 'Введите пароль',
+                            validator: ValidatorUtil.validateNotEmptyAndLettersAndNumber
+                        },
+                        {
+                            xtype: 'eyebutton',
+                            handler: 'viewPasswordClick'
+                        }
+                    ]
                 },
                 {
-                    xtype: 'button',
-                    iconCls: 'fas fa-eye noColor-noBorderBtn',
-                    cls: 'noColor-noBorderBtn',
-                    handler: function (btn) {
-                        let val = btn.up('container').items.items[0].inputEl.dom.type
-                        if (val === 'password') {
-                            btn.up('container').items.items[0].inputEl.dom.type = 'text'
-                            this.setIconCls('fas fa-eye-slash noColor-noBorderBtn')
-                        } else {
-                            btn.up('container').items.items[0].inputEl.dom.type = 'password'
-                            this.setIconCls('fas fa-eye noColor-noBorderBtn')
+                    items: [
+                        {
+                            fieldLabel: 'Повторите пароль',
+                            emptyText: 'Введите пароль',
+                            validator: ValidatorUtil.validatePasswordConfirm
+                        },
+                        {
+                            xtype: 'eyebutton',
+                            handler: 'viewPasswordClick'
                         }
-                    }
+                    ]
                 }
             ]
-        },
-        {
-            xtype: 'savebutton',
-            handler: function (btn) {
-                let input = ExtUtil.idQuery('passwordInput')
-                if (!input.isValid()) {
-                    Ext.Msg.alert('Внимание', 'Неверно заполнена форма.')
-                    return
-                }
-                Ext.Ajax.request({
-                    url: '/web/registration/changePassword',
-                    params: {
-                        password: input.getValue()
-                    },
-                    success: function (rs) {
-                        let response = Ext.JSON.decode(rs.responseText)
-                        if (response.success) {
-                            Ext.Msg.alert('Сохранено', 'Пароль успешно обновлен.')
-                        } else {
-                            Ext.Msg.alert('Ошибка', 'Ошибки при обновлении пароля')
-                        }
-                    }
-                })
-            }
         }
     ]
 })
