b13b346be6b2521a481d4a1a23587ffbf2351e47
barilo <barilo>
2023-10-15 12:50:33 +0300

    payment requisites

--- src/main/resources/static/js/main/view/paymentTypes/CreatePaymentTypeController.js
+++ src/main/resources/static/js/main/view/paymentTypes/CreatePaymentTypeController.js
@@ -10,22 +10,43 @@ Ext.define('Main.view.paymentTypes.CreatePaymentTypeController', {
             ExtMessages.incorrectlyForm()
             return
         }
-        let jsonData = form.getValues()
-        let requisites = []
-        for (let record of Ext.getStore('createPaymentTypeRequisitesStore').getRange()) {
-            requisites.push(record.getData())
+        let isPaymentTypeOn = ExtUtil.idQuery('isOnCheckbox').getValue()
+        let saveFn = function () {
+            let jsonData = form.getValues()
+            let requisites = []
+            for (let record of Ext.getStore('createPaymentTypeRequisitesStore').getRange()) {
+                requisites.push(record.getData())
+            }
+            jsonData.requisites = requisites
+            ExtUtil.request({
+                url: '/web/paymentTypes/save',
+                jsonData: jsonData,
+                success: function (response) {
+                    Ext.getStore('paymentTypesStore').reload()
+                    win.setLoading(false)
+                    Ext.Msg.alert('Информация', 'Тип оплаты <b>' + response.body.data.name + '</b> успешно сохранен.')
+                    ExtUtil.closeWindow(me)
+                },
+                loadingComponent: win
+            })
         }
-        jsonData.requisites = requisites
-        ExtUtil.request({
-            url: '/web/paymentTypes/save',
-            jsonData: jsonData,
-            success: function (response) {
-                Ext.getStore('paymentTypesStore').reload()
-                win.setLoading(false)
-                Ext.Msg.alert('Информация', 'Тип оплаты <b>' + response.body.data.name + '</b> успешно сохранен.')
-                ExtUtil.closeWindow(me)
-            },
-            loadingComponent: win
-        })
+        if (isPaymentTypeOn && Ext.getStore('createPaymentTypeRequisitesStore').getRange()
+            .filter(rec => rec.get('isOn') === true)
+            .length === 0) {
+            Ext.Msg.show({
+                title: 'Удаление пользователя',
+                message: 'Тип оплаты включен, но нет ни одного </br>включенного реквизита,что приведет к ошибке </br>при оформлении сделки. Желаете продолжить?',
+                buttons: Ext.Msg.YESNO,
+                icon: Ext.Msg.QUESTION,
+                fn: function (btn) {
+                    if (btn === 'yes') {
+                        saveFn()
+                    } else {
+                        win.setLoading(false)
+                    }
+                }
+            });
+        } else saveFn()
+
     }
 })
