9d52ff04579c80656a6b50ac60d7c0eea1ec55e8
barilo <e.barilo@isida.by>
2024-01-04 10:46:23 +0300

    удаление сделки при выходе из её оформления

--- src/main/java/tgb/btc/rce/service/processors/Start.java
+++ src/main/java/tgb/btc/rce/service/processors/Start.java
@@ -3,16 +3,26 @@ package tgb.btc.rce.service.processors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import tgb.btc.library.constants.enums.bot.BotMessageType;
+import tgb.btc.library.repository.bot.DealRepository;
 import tgb.btc.rce.annotation.CommandProcessor;
 import tgb.btc.rce.enums.Command;
 import tgb.btc.rce.service.Processor;
 import tgb.btc.rce.service.impl.BotMessageService;
 import tgb.btc.rce.util.UpdateUtil;
 
+import java.util.Objects;
+
 @CommandProcessor(command = Command.START)
 public class Start extends Processor {
 
     private BotMessageService botMessageService;
+
+    private DealRepository dealRepository;
+
+    @Autowired
+    public void setDealRepository(DealRepository dealRepository) {
+        this.dealRepository = dealRepository;
+    }
 
     @Autowired
     public void setBotMessageService(BotMessageService botMessageService) {
@@ -28,6 +38,13 @@ public class Start extends Processor {
     public void run(Long chatId) {
         userService.updateIsActiveByChatId(true, chatId);
         responseSender.sendBotMessage(botMessageService.findByType(BotMessageType.START), chatId);
+        Long currentDealPid = userRepository.getCurrentDealByChatId(chatId);
+        if (Objects.nonNull(currentDealPid)) {
+            if (dealRepository.existsById(currentDealPid)) {
+                dealRepository.deleteById(currentDealPid);
+            }
+            userRepository.updateCurrentDealByChatId(null, chatId);
+        }
         processToMainMenu(chatId);
     }
 }
