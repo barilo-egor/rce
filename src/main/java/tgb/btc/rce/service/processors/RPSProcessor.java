ab038658fb5b98a47902fffcd2c0d844c70a1af1
hodikov <hodikov@isida.by>
2024-02-01 18:13:49 +0300

    rps

--- src/main/java/tgb/btc/rce/service/processors/RPSProcessor.java
+++ src/main/java/tgb/btc/rce/service/processors/RPSProcessor.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import tgb.btc.library.constants.enums.properties.PropertiesPath;
+import tgb.btc.library.service.process.RPSService;
 import tgb.btc.rce.annotation.CommandProcessor;
 import tgb.btc.rce.enums.Command;
 import tgb.btc.rce.service.Processor;
@@ -11,6 +12,7 @@ import tgb.btc.rce.service.impl.KeyboardService;
 import tgb.btc.rce.util.UpdateUtil;
 
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static tgb.btc.library.constants.enums.properties.PropertiesPath.RPS_MESSAGE;
 
@@ -19,31 +21,57 @@ public class RPSProcessor extends Processor {
 
     private KeyboardService keyboardService;
 
+    private RPSService rpsService;
+
+    public static ConcurrentHashMap<Long, String> localCache = new ConcurrentHashMap<>();
+
     @Autowired
     public void setKeyboardService(KeyboardService keyboardService) {
         this.keyboardService = keyboardService;
     }
 
+    @Autowired
+    public void setRpsService(RPSService rpsService) {
+        this.rpsService = rpsService;
+    }
+
+
     public void run(Update update) {
         Long chatId = UpdateUtil.getChatId(update);
+        CallbackQuery query;
+        Integer referralBalance;
         switch (userRepository.getStepByChatId(chatId)) {
             case 0:
-                responseSender.sendMessage(chatId, RPS_MESSAGE.getString("start"), keyboardService.getRPSRates());
+                referralBalance = userService.getReferralBalanceByChatId(chatId);
+                responseSender.sendMessage(chatId,
+                        String.format(RPS_MESSAGE.getString("start"),
+                                String.format(RPS_MESSAGE.getString("referral.balance"), referralBalance)),
+                        keyboardService.getRPSRates());
                 userRepository.updateCommandByChatId(Command.RPS.name(), chatId);
                 userRepository.updateStepByChatId(chatId, 1);
                 break;
             case 1:
-                CallbackQuery query = update.getCallbackQuery();
+                 query = update.getCallbackQuery();
                 if (Objects.nonNull(query)) {
-                    String data = query.getData();
                     responseSender.deleteCallbackMessageIfExists(update);
-                    if (!data.equals(PropertiesPath.RPS_MESSAGE.getString("close"))) {
+                    if (!query.getData().equals(PropertiesPath.RPS_MESSAGE.getString("close"))) {
+                        localCache.put(chatId, query.getData());
                         responseSender.sendMessage(chatId, RPS_MESSAGE.getString("ask"),keyboardService.getRPSElements());
-                    } else {
-
+                        userRepository.updateStepByChatId(chatId, 2);
                     }
+                }
+                break;
+            case 2:
+                query = update.getCallbackQuery();
+                if (Objects.nonNull(query)) {
+                    responseSender.deleteCallbackMessageIfExists(update);
+                    StringBuilder sb = new StringBuilder(rpsService.getResultMessageText(query.getData(), localCache.get(chatId), chatId));
+                    referralBalance = userService.getReferralBalanceByChatId(chatId);
+                    sb.append(System.lineSeparator())
+                            .append(String.format(RPS_MESSAGE.getString("referral.balance"), referralBalance));
+                    responseSender.sendMessage(chatId, sb.toString());
                 } else {
-                    responseSender.sendMessage(chatId, RPS_MESSAGE.getString("start"), keyboardService.getRPSRates());
+                    responseSender.sendMessage(chatId, RPS_MESSAGE.getString("ask"),keyboardService.getRPSElements());
                 }
         }
     }
