f6aecd3635ad036a978b5d7190c7383bdf615c3a
barilo <e.barilo@isida.by>
2024-06-25 10:32:14 +0300

    remove games

--- src/main/java/tgb/btc/rce/enums/Menu.java
+++ src/main/java/tgb/btc/rce/enums/Menu.java
@@ -9,7 +9,7 @@ public enum Menu {
     MAIN(ReferralType.STANDARD.isCurrent()
             ? List.of(Command.BUY_BITCOIN, Command.SELL_BITCOIN, Command.CONTACTS, Command.DRAWS, Command.REFERRAL)
             : List.of(Command.BUY_BITCOIN, Command.SELL_BITCOIN, Command.CONTACTS, Command.DRAWS)),
-    DRAWS(List.of(Command.DICE, Command.SLOT_REEL, Command.LOTTERY, Command.ROULETTE, Command.RPS, Command.BACK)),
+    DRAWS(List.of(Command.LOTTERY, Command.ROULETTE, Command.BACK)),
     ASK_CONTACT(List.of(Command.SHARE_CONTACT, Command.CANCEL)),
     ADMIN_PANEL(List.of(Command.REQUESTS, Command.BOT_SETTINGS, Command.REPORTS, Command.DISCOUNTS,
             Command.USERS, Command.DEALS_COUNT, Command.QUIT_ADMIN_PANEL)),
