aa30addbe37180eff294210631373cc3cd01a2e9
barilo <e.barilo@isida.by>
2024-06-28 17:52:12 +0300

    dynamic requisite services refactor

--- src/main/java/tgb/btc/rce/service/processors/paymenttypes/requisite/dynamic/TurningDynamic.java
+++ src/main/java/tgb/btc/rce/service/processors/paymenttypes/requisite/dynamic/TurningDynamic.java
@@ -4,11 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import tgb.btc.library.bean.bot.PaymentRequisite;
 import tgb.btc.library.bean.bot.PaymentType;
+import tgb.btc.library.interfaces.service.bean.bot.IPaymentRequisiteService;
+import tgb.btc.library.interfaces.service.bean.bot.IPaymentTypeService;
 import tgb.btc.rce.annotation.CommandProcessor;
 import tgb.btc.rce.constants.BotStringConstants;
 import tgb.btc.rce.enums.Command;
-import tgb.btc.library.repository.bot.PaymentRequisiteRepository;
-import tgb.btc.library.repository.bot.PaymentTypeRepository;
 import tgb.btc.rce.service.Processor;
 import tgb.btc.rce.util.UpdateUtil;
 
@@ -17,25 +17,25 @@ import java.util.List;
 @CommandProcessor(command = Command.TURNING_DYNAMIC_REQUISITES)
 public class TurningDynamic extends Processor {
 
-    private PaymentTypeRepository paymentTypeRepository;
+    private IPaymentTypeService paymentTypeService;
 
-    private PaymentRequisiteRepository paymentRequisiteRepository;
+    private IPaymentRequisiteService paymentRequisiteService;
 
     private TurnDynamicRequisites turnDynamicRequisites;
 
     @Autowired
+    public void setPaymentTypeService(IPaymentTypeService paymentTypeService) {
+        this.paymentTypeService = paymentTypeService;
+    }
+
+    @Autowired
+    public void setPaymentRequisiteService(IPaymentRequisiteService paymentRequisiteService) {
+        this.paymentRequisiteService = paymentRequisiteService;
+    }
+
+    @Autowired
     public void setTurnDynamicRequisites(TurnDynamicRequisites turnDynamicRequisites) {
         this.turnDynamicRequisites = turnDynamicRequisites;
-    }
-
-    @Autowired
-    public void setPaymentRequisiteRepository(PaymentRequisiteRepository paymentRequisiteRepository) {
-        this.paymentRequisiteRepository = paymentRequisiteRepository;
-    }
-
-    @Autowired
-    public void setPaymentTypeRepository(PaymentTypeRepository paymentTypeRepository) {
-        this.paymentTypeRepository = paymentTypeRepository;
     }
 
     @Override
@@ -47,20 +47,20 @@ public class TurningDynamic extends Processor {
         String[] values = update.getCallbackQuery().getData().split(BotStringConstants.CALLBACK_DATA_SPLITTER);
         boolean value = Boolean.parseBoolean(values[2]);
         Long pid = Long.parseLong(values[1]);
-        PaymentType paymentType = paymentTypeRepository.getByPid(pid);
+        PaymentType paymentType = paymentTypeService.getByPid(pid);
         if (!value) {
-            paymentTypeRepository.updateIsDynamicOnByPid(false, pid);
+            paymentTypeService.updateIsDynamicOnByPid(false, pid);
             responseSender.deleteMessage(chatId, update.getCallbackQuery().getMessage().getMessageId());
             responseSender.sendMessage(chatId, "Динамические реквизиты для " + paymentType.getName() + " выключены.");
             turnDynamicRequisites.sendPaymentTypes(chatId, paymentType.getDealType(), paymentType.getFiatCurrency());
         } else {
-            List<PaymentRequisite> paymentRequisites = paymentRequisiteRepository.getByPaymentType_Pid(pid);
+            List<PaymentRequisite> paymentRequisites = paymentRequisiteService.getByPaymentType_Pid(pid);
             if (paymentRequisites.size() <= 1) {
                 responseSender.sendMessage(chatId, "Недостаточно реквизитов для включения. Количество реквизитов: "
                         + paymentRequisites.size() + ".");
                 return;
             }
-            paymentTypeRepository.updateIsDynamicOnByPid(true, pid);
+            paymentTypeService.updateIsDynamicOnByPid(true, pid);
             responseSender.deleteMessage(chatId, update.getCallbackQuery().getMessage().getMessageId());
             responseSender.sendMessage(chatId, "Динамические реквизиты для " + paymentType.getName() + " включены.");
             turnDynamicRequisites.sendPaymentTypes(chatId, paymentType.getDealType(), paymentType.getFiatCurrency());
