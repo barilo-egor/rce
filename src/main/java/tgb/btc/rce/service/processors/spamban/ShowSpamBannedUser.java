aa109cc7ab5ac5fa7b78eaa9b40444f89740d3a4
barilo <e.barilo@isida.by>
2024-06-28 18:19:11 +0300

    spam ban services refactor

--- src/main/java/tgb/btc/rce/service/processors/spamban/ShowSpamBannedUser.java
+++ src/main/java/tgb/btc/rce/service/processors/spamban/ShowSpamBannedUser.java
@@ -2,12 +2,12 @@ package tgb.btc.rce.service.processors.spamban;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import tgb.btc.library.interfaces.service.bean.bot.ISpamBanService;
+import tgb.btc.library.service.bean.bot.UserService;
 import tgb.btc.rce.annotation.CommandProcessor;
 import tgb.btc.rce.enums.Command;
-import tgb.btc.library.repository.bot.SpamBanRepository;
 import tgb.btc.rce.service.Processor;
 import tgb.btc.rce.service.impl.UserInfoService;
-import tgb.btc.library.service.bean.bot.UserService;
 import tgb.btc.rce.util.CallbackQueryUtil;
 import tgb.btc.rce.util.UpdateUtil;
 
@@ -16,11 +16,11 @@ public class ShowSpamBannedUser extends Processor {
 
     private UserInfoService userInfoService;
 
-    private SpamBanRepository spamBanRepository;
+    private ISpamBanService spamBanService;
 
     @Autowired
-    public void setSpamBanRepository(SpamBanRepository spamBanRepository) {
-        this.spamBanRepository = spamBanRepository;
+    public void setSpamBanService(ISpamBanService spamBanService) {
+        this.spamBanService = spamBanService;
     }
 
     @Autowired
@@ -38,7 +38,7 @@ public class ShowSpamBannedUser extends Processor {
         Long spamBanPid = CallbackQueryUtil.getSplitLongData(update, 1);
         Long chatId = UpdateUtil.getChatId(update);
         responseSender.deleteMessage(chatId, update.getCallbackQuery().getMessage().getMessageId());
-        if (spamBanRepository.countByPid(spamBanPid) == 0) {
+        if (spamBanService.countByPid(spamBanPid) == 0) {
             responseSender.sendMessage(chatId, "Заявка уже обработана.");
             return;
         }
