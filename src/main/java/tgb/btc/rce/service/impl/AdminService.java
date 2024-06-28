f651ff24fe1902018553291c2941c6804d0b17cb
barilo <e.barilo@isida.by>
2024-06-28 15:19:47 +0300

    AdminService services refactor

--- src/main/java/tgb/btc/rce/service/impl/AdminService.java
+++ src/main/java/tgb/btc/rce/service/impl/AdminService.java
@@ -3,7 +3,7 @@ package tgb.btc.rce.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
-import tgb.btc.library.service.bean.bot.UserService;
+import tgb.btc.library.interfaces.service.bot.user.IReadUserService;
 import tgb.btc.rce.enums.BotInlineButton;
 import tgb.btc.rce.service.sender.ResponseSender;
 import tgb.btc.rce.vo.InlineButton;
@@ -11,27 +11,32 @@ import tgb.btc.rce.vo.InlineButton;
 @Service
 public class AdminService {
 
-    private final UserService userService;
-    private final ResponseSender responseSender;
+    private IReadUserService readUserService;
+
+    private ResponseSender responseSender;
 
     @Autowired
-    public AdminService(UserService userService, ResponseSender responseSender) {
-        this.userService = userService;
+    public void setReadUserService(IReadUserService readUserService) {
+        this.readUserService = readUserService;
+    }
+
+    @Autowired
+    public void setResponseSender(ResponseSender responseSender) {
         this.responseSender = responseSender;
     }
 
     public void notify(String message) {
-        userService.getAdminsChatIds().forEach(chatId -> responseSender.sendMessage(chatId, message));
+        readUserService.getAdminsChatIds().forEach(chatId -> responseSender.sendMessage(chatId, message));
     }
 
     public void notify(String message, ReplyKeyboard replyKeyboard) {
-        userService.getAdminsChatIds().forEach(chatId -> responseSender.sendMessage(chatId, message, replyKeyboard));
+        readUserService.getAdminsChatIds().forEach(chatId -> responseSender.sendMessage(chatId, message, replyKeyboard));
     }
 
     public void notify(String message, String data) {
         InlineButton button = BotInlineButton.SHOW.getButton();
         button.setData(data);
-        userService.getAdminsChatIds().forEach(chatId ->
+        readUserService.getAdminsChatIds().forEach(chatId ->
                 responseSender.sendMessage(chatId, message, button));
     }
 }
