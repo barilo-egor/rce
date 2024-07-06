a6a2d967aab470be1974b72722c9aa952427274e
barilo <barilo>
2024-07-06 18:31:50 +0300

    make operator

---
+++ src/main/java/tgb/btc/rce/service/impl/processors/role/ChangeRoleProcessor.java
@@ -0,0 +1,28 @@
+package tgb.btc.rce.service.impl.processors.role;
+
+import org.slf4j.Logger;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import tgb.btc.library.constants.enums.bot.UserRole;
+import tgb.btc.rce.service.Processor;
+import tgb.btc.rce.util.UpdateUtil;
+
+public abstract class ChangeRoleProcessor extends Processor {
+
+    protected void changeRole(Update update, UserRole userRole) {
+        Long chatId = UpdateUtil.getChatId(update);
+        Long userChatId = Long.parseLong(UpdateUtil.getMessageText(update).split(" ")[1]);
+        if (!readUserService.existsByChatId(userChatId)) {
+            responseSender.sendMessage(chatId, "Пользователь с таким ID не найден.");
+            return;
+        }
+        if (readUserService.getUserRoleByChatId(chatId).equals(userRole)) {
+            responseSender.sendMessage(chatId, "Пользователь " + userChatId + " уже в роли \"" + userRole.getDisplayName() + "\".");
+            return;
+        }
+        modifyUserService.updateUserRoleByChatId(userRole, userChatId);
+        responseSender.sendMessage(chatId, "Пользователю " + userChatId + " сменена роль на \"" + userRole.getDisplayName() + "\".");
+        getLogger().debug("Админ {} сменил пользователю {} роль на {}.", chatId, userChatId, userRole.getDisplayName());
+    }
+
+    protected abstract Logger getLogger();
+}
