810c3928af520e62fe54646ac286107974b4a121
barilo <e.barilo@isida.by>
2024-07-12 10:46:01 +0300

    BotImageUtilTest

--- src/main/java/tgb/btc/rce/util/BotImageUtil.java
+++ src/main/java/tgb/btc/rce/util/BotImageUtil.java
@@ -1,17 +1,24 @@
 package tgb.btc.rce.util;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.telegram.telegrambots.meta.api.objects.PhotoSize;
+import tgb.btc.library.exception.BaseException;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 
 public final class BotImageUtil {
     private BotImageUtil() {
     }
 
     public static String getImageId(List<PhotoSize> image) {
-        return Objects.requireNonNull(image.stream().max(Comparator.comparing(PhotoSize::getFileSize))
-                .orElse(null)).getFileId();
+        if (CollectionUtils.isEmpty(image))
+            throw new BaseException("Для поиска максимального размера фото передана пустая коллекция либо null.");
+        Optional<PhotoSize> optionalPhotoSize = image.stream()
+                .max(Comparator.comparing(PhotoSize::getFileSize));
+        if (optionalPhotoSize.isEmpty())
+            throw new BaseException("Не найден ни один PhotoSize с максимальным fileSize.");
+        return optionalPhotoSize.get().getFileId();
     }
 }
---
+++ src/test/java/tgb/btc/rce/util/BotImageUtilTest.java
@@ -0,0 +1,41 @@
+package tgb.btc.rce.util;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+import org.junit.jupiter.api.Test;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
+import tgb.btc.library.exception.BaseException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class BotImageUtilTest {
+
+    @Test
+    void getImageId() {
+        int numberOfTries = 3;
+        for (int i = 1; i <= numberOfTries; i++) {
+            List<PhotoSize> photoSizeList = new ArrayList<>();
+            PhotoSize maxPhotoSize = new PhotoSize();
+            int randomMaxSize = RandomUtils.nextInt(1000, 9999);
+            maxPhotoSize.setFileSize(randomMaxSize);
+            String randomFileId = RandomStringUtils.randomAlphanumeric(10);
+            maxPhotoSize.setFileId(randomFileId);
+            photoSizeList.add(maxPhotoSize);
+            PhotoSize photoSize = new PhotoSize();
+            photoSize.setFileSize(RandomUtils.nextInt(1, 999));
+            photoSizeList.add(photoSize);
+            assertEquals(randomFileId, BotImageUtil.getImageId(photoSizeList));
+        }
+    }
+
+    @Test
+    void getImageIdThrows() {
+        assertThrows(BaseException.class, () -> BotImageUtil.getImageId(new ArrayList<>()));
+        assertThrows(BaseException.class, () -> BotImageUtil.getImageId(null));
+    }
+
+}
