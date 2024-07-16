812a6715ef0f2757ac5edfeb8a62df94c6cc07d2
barilo <e.barilo@isida.by>
2024-07-16 18:09:46 +0300

    remove ReviewPriseUtil.java

--- src/main/java/tgb/btc/rce/service/impl/process/ReviewPriseProcessService.java
+++ src/main/java/tgb/btc/rce/service/impl/process/ReviewPriseProcessService.java
@@ -1,27 +1,34 @@
 package tgb.btc.rce.service.impl.process;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import tgb.btc.api.library.IReviewPriseProcessService;
 import tgb.btc.library.bean.bot.Deal;
 import tgb.btc.library.constants.enums.ReferralType;
+import tgb.btc.library.constants.enums.bot.FiatCurrency;
+import tgb.btc.library.constants.enums.properties.PropertiesPath;
 import tgb.btc.library.constants.enums.properties.VariableType;
+import tgb.btc.library.exception.PropertyValueNotFoundException;
 import tgb.btc.library.interfaces.service.bean.bot.deal.IReadDealService;
 import tgb.btc.library.util.properties.VariablePropertiesUtil;
 import tgb.btc.rce.enums.Command;
 import tgb.btc.rce.enums.ReviewPriseType;
 import tgb.btc.rce.service.IResponseSender;
+import tgb.btc.rce.service.IReviewPriseBotProcessService;
 import tgb.btc.rce.service.keyboard.IKeyboardBuildService;
 import tgb.btc.rce.service.util.ICallbackQueryService;
-import tgb.btc.rce.util.ReviewPriseUtil;
 import tgb.btc.rce.vo.InlineButton;
 import tgb.btc.rce.vo.ReviewPrise;
 
-import java.util.List;
-import java.util.Objects;
+import javax.annotation.PostConstruct;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
-public class ReviewPriseProcessService implements IReviewPriseProcessService {
+public class ReviewPriseProcessService implements tgb.btc.api.library.IReviewPriseProcessService, IReviewPriseBotProcessService {
+
+    private final List<ReviewPrise> REVIEW_PRISES = new ArrayList<>();
 
     private IResponseSender responseSender;
 
@@ -30,6 +37,13 @@ public class ReviewPriseProcessService implements IReviewPriseProcessService {
     private IKeyboardBuildService keyboardBuildService;
 
     private ICallbackQueryService callbackQueryService;
+
+    private IReviewPriseBotProcessService reviewPriseBotProcessService;
+
+    @Autowired
+    public void setReviewPriseBotProcessService(IReviewPriseBotProcessService reviewPriseBotProcessService) {
+        this.reviewPriseBotProcessService = reviewPriseBotProcessService;
+    }
 
     @Autowired
     public void setCallbackQueryService(ICallbackQueryService callbackQueryService) {
@@ -58,7 +72,7 @@ public class ReviewPriseProcessService implements IReviewPriseProcessService {
             String data;
             String amount;
             if (ReviewPriseType.DYNAMIC.isCurrent()) {
-                ReviewPrise reviewPriseVo = ReviewPriseUtil.getReviewPrise(deal.getAmount(), deal.getFiatCurrency());
+                ReviewPrise reviewPriseVo = reviewPriseBotProcessService.getReviewPrise(deal.getAmount(), deal.getFiatCurrency());
                 if (Objects.nonNull(reviewPriseVo)) {
                     data = callbackQueryService.buildCallbackData(Command.SHARE_REVIEW,
                             new Object[]{String.valueOf(reviewPriseVo.getMinPrise()), String.valueOf(reviewPriseVo.getMaxPrise())});
@@ -80,5 +94,52 @@ public class ReviewPriseProcessService implements IReviewPriseProcessService {
                             )
                     ));
         }
+    }
+
+    @Override
+    public ReviewPrise getReviewPrise(BigDecimal sum, FiatCurrency fiatCurrency) {
+        for (ReviewPrise reviewPrise : REVIEW_PRISES.stream()
+                .filter(reviewPrise -> reviewPrise.getFiatCurrency().equals(fiatCurrency))
+                .collect(Collectors.toList())) {
+            if (BigDecimal.valueOf(reviewPrise.getSum()).compareTo(sum) < 1)
+                return reviewPrise;
+        }
+        return null;
+    }
+
+    @PostConstruct
+    private void load() {
+        REVIEW_PRISES.clear();
+        for (String key : PropertiesPath.REVIEW_PRISE_PROPERTIES.getKeys()) {
+            int sum;
+            if (StringUtils.isBlank(key)) {
+                throw new PropertyValueNotFoundException("Не указано название для одного из ключей" + key + ".");
+            }
+            try {
+                sum = Integer.parseInt(key.split("\\.")[1]);
+            } catch (NumberFormatException e) {
+                throw new PropertyValueNotFoundException("Не корректное название для ключа " + key + ".");
+            }
+            String[] priseValues = PropertiesPath.REVIEW_PRISE_PROPERTIES.getStringArray(key);
+            if (priseValues.length == 0) {
+                throw new PropertyValueNotFoundException("Не указано значение для ключа " + key + ".");
+            }
+            int minPrise;
+            int maxPrise;
+            try {
+                minPrise = Integer.parseInt(priseValues[0]);
+                maxPrise = Integer.parseInt(priseValues[1]);
+            } catch (NumberFormatException e) {
+                throw new PropertyValueNotFoundException("Не корректное значение для ключа " + key + ".");
+            }
+            REVIEW_PRISES.add(ReviewPrise.builder()
+                    .minPrise(minPrise)
+                    .maxPrise(maxPrise)
+                    .sum(sum)
+                    .fiatCurrency(FiatCurrency.getByCode(key.split("\\.")[0]))
+                    .build());
+        }
+        REVIEW_PRISES.sort(Comparator.comparingInt(ReviewPrise::getSum));
+        Collections.reverse(REVIEW_PRISES);
     }
 }
