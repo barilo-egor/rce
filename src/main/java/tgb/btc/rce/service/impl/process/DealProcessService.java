0f3df9adbec0eebee5f65cf59bdfe79a5d600d1b
barilo <e.barilo@isida.by>
2024-07-16 16:59:03 +0300

    DealPromoUtil.java remove

--- src/main/java/tgb/btc/rce/service/impl/process/DealProcessService.java
+++ src/main/java/tgb/btc/rce/service/impl/process/DealProcessService.java
@@ -3,13 +3,21 @@ package tgb.btc.rce.service.impl.process;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tgb.btc.library.interfaces.service.bean.common.bot.IDealCommonService;
+import tgb.btc.library.service.properties.ModulesPropertiesReader;
+import tgb.btc.rce.enums.FirstDealPromoType;
 import tgb.btc.rce.service.process.IDealProcessService;
-import tgb.btc.rce.util.DealPromoUtil;
 
 @Service
 public class DealProcessService implements IDealProcessService {
 
     private IDealCommonService dealCommonService;
+
+    private ModulesPropertiesReader modulesPropertiesReader;
+
+    @Autowired
+    public void setModulesPropertiesReader(ModulesPropertiesReader modulesPropertiesReader) {
+        this.modulesPropertiesReader = modulesPropertiesReader;
+    }
 
     @Autowired
     public void setDealCommonService(IDealCommonService dealCommonService) {
@@ -17,6 +25,8 @@ public class DealProcessService implements IDealProcessService {
     }
 
     public boolean isAvailableForPromo(Long chatId) {
-        return !DealPromoUtil.isNone() && dealCommonService.isFirstDeal(chatId);
+        return !(FirstDealPromoType.NONE.equals(FirstDealPromoType.valueOf(
+                modulesPropertiesReader.getString("first.deal.promo.type"))))
+                && dealCommonService.isFirstDeal(chatId);
     }
 }
