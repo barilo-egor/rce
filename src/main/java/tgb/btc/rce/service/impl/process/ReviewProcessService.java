1644dec6fb43e2ffe5f19bff64bb1bff925418ce
barilo <e.barilo@isida.by>
2024-08-15 18:53:05 +0300

    publish review

---
+++ src/main/java/tgb/btc/rce/service/impl/process/ReviewProcessService.java
@@ -0,0 +1,66 @@
+package tgb.btc.rce.service.impl.process;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tgb.btc.library.bean.bot.Review;
+import tgb.btc.library.constants.enums.properties.VariableType;
+import tgb.btc.library.interfaces.IModule;
+import tgb.btc.library.interfaces.service.bean.bot.IReviewService;
+import tgb.btc.library.interfaces.service.bean.bot.user.IModifyUserService;
+import tgb.btc.library.interfaces.service.bean.bot.user.IReadUserService;
+import tgb.btc.library.service.properties.VariablePropertiesReader;
+import tgb.btc.rce.enums.ReviewPriseType;
+import tgb.btc.rce.sender.IResponseSender;
+import tgb.btc.rce.service.process.IReviewProcessService;
+
+import static tgb.btc.rce.enums.ReviewPriseType.DYNAMIC;
+
+@Service
+@Slf4j
+public class ReviewProcessService implements IReviewProcessService {
+
+    private final VariablePropertiesReader variablePropertiesReader;
+
+    private final IReviewService reviewService;
+
+    private final IResponseSender responseSender;
+
+    private final IModule<ReviewPriseType> reviewPriseModule;
+
+    private final IReadUserService readUserService;
+
+    private final IModifyUserService modifyUserService;
+
+    @Autowired
+    public ReviewProcessService(VariablePropertiesReader variablePropertiesReader, IReviewService reviewService,
+                                IResponseSender responseSender, IModule<ReviewPriseType> reviewPriseModule,
+                                IReadUserService readUserService, IModifyUserService modifyUserService) {
+        this.variablePropertiesReader = variablePropertiesReader;
+        this.reviewService = reviewService;
+        this.responseSender = responseSender;
+        this.reviewPriseModule = reviewPriseModule;
+        this.readUserService = readUserService;
+        this.modifyUserService = modifyUserService;
+    }
+
+    @Override
+    public void publish(Long pid) {
+        Long channelChatId = Long.parseLong(variablePropertiesReader.getVariable(VariableType.CHANNEL_CHAT_ID));
+        Review review = reviewService.findById(pid);
+        responseSender.sendMessage(channelChatId, review.getText());
+        review.setPublished(true);
+        reviewService.save(review);
+        Integer reviewPrise = reviewPriseModule.isCurrent(DYNAMIC)
+                ? review.getAmount()
+                : variablePropertiesReader.getInt(VariableType.REVIEW_PRISE);
+        Integer referralBalance = readUserService.getReferralBalanceByChatId(review.getChatId());
+        int total = referralBalance + reviewPrise;
+
+        log.debug("Обновление реф баланса за отзыв : chatId = " + review.getChatId() + "; reviewPrise = "
+                + reviewPrise + "; referralBalance = " + referralBalance + "; total = " + total);
+        modifyUserService.updateReferralBalanceByChatId(total, review.getChatId());
+        responseSender.sendMessage(review.getChatId(), "Ваш отзыв опубликован.\n\nНа ваш реферальный баланс зачислено "
+                + reviewPrise + "₽.");
+    }
+}
--- src/main/java/tgb/btc/rce/service/processors/admin/requests/review/PublishReview.java
+++ src/main/java/tgb/btc/rce/service/processors/admin/requests/review/PublishReview.java
@@ -3,69 +3,33 @@ package tgb.btc.rce.service.processors.admin.requests.review;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import tgb.btc.library.bean.bot.Review;
-import tgb.btc.library.constants.enums.properties.VariableType;
-import tgb.btc.library.interfaces.IModule;
-import tgb.btc.library.interfaces.service.bean.bot.IReviewService;
-import tgb.btc.library.service.properties.VariablePropertiesReader;
 import tgb.btc.rce.annotation.CommandProcessor;
 import tgb.btc.rce.constants.BotStringConstants;
 import tgb.btc.rce.enums.Command;
-import tgb.btc.rce.enums.ReviewPriseType;
 import tgb.btc.rce.service.Processor;
-
-import static tgb.btc.rce.enums.ReviewPriseType.DYNAMIC;
+import tgb.btc.rce.service.process.IReviewProcessService;
 
 @CommandProcessor(command = Command.PUBLISH_REVIEW)
 @Slf4j
 public class PublishReview extends Processor {
 
-    private IReviewService reviewService;
-
-    private VariablePropertiesReader variablePropertiesReader;
-
-    private IModule<ReviewPriseType> reviewPriseModule;
+    private final IReviewProcessService reviewProcessService;
 
     @Autowired
-    public void setReviewPriseModule(IModule<ReviewPriseType> reviewPriseModule) {
-        this.reviewPriseModule = reviewPriseModule;
-    }
-
-    @Autowired
-    public void setVariablePropertiesReader(VariablePropertiesReader variablePropertiesReader) {
-        this.variablePropertiesReader = variablePropertiesReader;
-    }
-
-    @Autowired
-    public void setReviewService(IReviewService reviewService) {
-        this.reviewService = reviewService;
+    public PublishReview(IReviewProcessService reviewProcessService) {
+        this.reviewProcessService = reviewProcessService;
     }
 
     @Override
     public void run(Update update) {
-        Long channelChatId = Long.parseLong(variablePropertiesReader.getVariable(VariableType.CHANNEL_CHAT_ID));
-
-        Review review = reviewService.findById(Long.parseLong(
-                update.getCallbackQuery().getData().split(BotStringConstants.CALLBACK_DATA_SPLITTER)[1]));
-
-//        String username = StringUtils.isEmpty(review.getUsername()) ? StringUtils.EMPTY
-//                : "\nОтзыв от @" + review.getUsername();
-        responseSender.sendMessage(channelChatId, review.getText());
         Long chatId = updateService.getChatId(update);
+
+        Long reviewPid = Long.parseLong(
+                update.getCallbackQuery().getData().split(BotStringConstants.CALLBACK_DATA_SPLITTER)[1]);
+        reviewProcessService.publish(reviewPid);
+        log.debug("Админ {} опубликовал отзыв {}", chatId, reviewPid);
+
         responseSender.deleteMessage(chatId, update.getCallbackQuery().getMessage().getMessageId());
         responseSender.sendMessage(chatId, "Отзыв опубликован.");
-        review.setPublished(true);
-        reviewService.save(review);
-        Integer reviewPrise = reviewPriseModule.isCurrent(DYNAMIC)
-                ? review.getAmount()
-                : variablePropertiesReader.getInt(VariableType.REVIEW_PRISE);
-        Integer referralBalance = readUserService.getReferralBalanceByChatId(review.getChatId());
-        int total = referralBalance + reviewPrise;
-        log.debug("Админ {} опубликовал отзыв {}", chatId, review.toString());
-        log.debug("Обновление реф баланса за отзыв : chatId = " + review.getChatId() + "; reviewPrise = "
-                + reviewPrise + "; referralBalance = " + referralBalance + "; total = " + total);
-        modifyUserService.updateReferralBalanceByChatId(total, review.getChatId());
-        responseSender.sendMessage(review.getChatId(), "Ваш отзыв опубликован.\n\nНа ваш реферальный баланс зачислено "
-                + reviewPrise + "₽.");
     }
 }
