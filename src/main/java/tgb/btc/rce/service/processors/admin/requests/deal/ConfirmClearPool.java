29f0041d2c16a9c8f889114e878692e6db41b0c9
barilo <barilo>
2024-11-07 01:28:44 +0300

    добавление-получение-очищение пула

--- src/main/java/tgb/btc/rce/service/processors/admin/requests/deal/ConfirmClearPool.java
+++ src/main/java/tgb/btc/rce/service/processors/admin/requests/deal/ConfirmClearPool.java
@@ -1,27 +1,37 @@
 package tgb.btc.rce.service.processors.admin.requests.deal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import tgb.btc.library.constants.enums.bot.CryptoCurrency;
-import tgb.btc.library.interfaces.service.process.IDealPoolService;
+import tgb.btc.library.exception.ApiResponseErrorException;
+import tgb.btc.library.interfaces.web.ICryptoWithdrawalService;
 import tgb.btc.rce.annotation.CommandProcessor;
 import tgb.btc.rce.enums.Command;
 import tgb.btc.rce.service.Processor;
 
+import java.util.Optional;
+
 @CommandProcessor(command = Command.CONFIRM_CLEAR_POOL)
 public class ConfirmClearPool extends Processor {
 
-    private final IDealPoolService dealPoolService;
+    private final ICryptoWithdrawalService cryptoWithdrawalService;
 
     @Autowired
-    public ConfirmClearPool(IDealPoolService dealPoolService) {
-        this.dealPoolService = dealPoolService;
+    public ConfirmClearPool(ICryptoWithdrawalService cryptoWithdrawalService) {
+        this.cryptoWithdrawalService = cryptoWithdrawalService;
     }
 
     @Override
     public void run(Update update) {
         Long chatId = updateService.getChatId(update);
-        dealPoolService.clearPool(CryptoCurrency.BITCOIN, chatId);
+        Optional<Message> processMessage = responseSender.sendMessage(chatId, "Пул в процессе очищения, пожалуйста подождите");
+        try {
+            cryptoWithdrawalService.clearPool();
+        } catch (ApiResponseErrorException e) {
+            responseSender.sendMessage(chatId, e.getMessage());
+            return;
+        }
+        processMessage.ifPresent(message -> responseSender.deleteMessage(chatId, message.getMessageId()));
         Integer poolMessageId = callbackQueryService.getSplitIntData(update, 1);
         responseSender.deleteMessage(chatId, poolMessageId);
         responseSender.deleteCallbackMessageIfExists(update);
