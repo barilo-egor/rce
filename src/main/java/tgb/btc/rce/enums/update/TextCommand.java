abdca9f0c9e59631ae4b943ca69d2662c92da838
barilo <e.barilo@isida.by>
2024-11-18 18:49:28 +0300

    draws

--- src/main/java/tgb/btc/rce/enums/update/TextCommand.java
+++ src/main/java/tgb/btc/rce/enums/update/TextCommand.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum TextCommand {
     BUY_BITCOIN("Купить"),
     SELL_BITCOIN("Продать"),
-    CONTACTS("Контакты");
+    CONTACTS("Контакты"),
+    DRAWS("Розыгрыши");
 
     private final String text;
 }
