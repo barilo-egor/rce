1ce6f4043542a89d12f0ebc0f0e98c7c1337963e
barilo <e.barilo@isida.by>
2024-11-26 18:46:43 +0300

    delete requisite

---
+++ src/main/java/tgb/btc/rce/service/handler/impl/callback/settings/FiatDeleteRequisiteHandler.java
@@ -0,0 +1,34 @@
+package tgb.btc.rce.service.handler.impl.callback.settings;
+
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import tgb.btc.library.constants.enums.bot.FiatCurrency;
+import tgb.btc.rce.enums.update.CallbackQueryData;
+import tgb.btc.rce.service.handler.callback.ICallbackQueryHandler;
+import tgb.btc.rce.service.handler.impl.message.text.command.settings.DeleteRequisiteHandler;
+import tgb.btc.rce.service.util.ICallbackDataService;
+
+@Service
+public class FiatDeleteRequisiteHandler implements ICallbackQueryHandler {
+
+    private final ICallbackDataService callbackDataService;
+
+    private final DeleteRequisiteHandler deleteRequisiteHandler;
+
+    public FiatDeleteRequisiteHandler(ICallbackDataService callbackDataService,
+                                      DeleteRequisiteHandler deleteRequisiteHandler) {
+        this.callbackDataService = callbackDataService;
+        this.deleteRequisiteHandler = deleteRequisiteHandler;
+    }
+
+    @Override
+    public void handle(CallbackQuery callbackQuery) {
+        FiatCurrency fiatCurrency = FiatCurrency.valueOf(callbackDataService.getArgument(callbackQuery.getData(), 1));
+        deleteRequisiteHandler.sendPaymentTypes(callbackQuery.getFrom().getId(), fiatCurrency, callbackQuery.getMessage().getMessageId());
+    }
+
+    @Override
+    public CallbackQueryData getCallbackQueryData() {
+        return CallbackQueryData.FIAT_DELETE_REQUISITE;
+    }
+}
