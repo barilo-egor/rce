131a204c3b030037dc171f72f55e73e302016700
barilo <e.barilo@isida.by>
2024-11-26 17:04:15 +0300

    create requisite

--- src/main/java/tgb/btc/rce/enums/RedisPrefix.java
+++ src/main/java/tgb/btc/rce/enums/RedisPrefix.java
@@ -9,7 +9,8 @@ public enum RedisPrefix {
     MESSAGE_ID("message_id_"),
     DEAL_PID("deal_pid_"),
     DEAL_TYPE("deal_type_"),
-    FIAT_CURRENCY("fiat_currency_");
+    FIAT_CURRENCY("fiat_currency_"),
+    PAYMENT_TYPE_PID("payment_type_pid_"),;
 
     final String prefix;
 }
