a0d27f3d3bce5f8ac6f89adfcec46893207c6963
barilo <e.barilo@isida.by>
2024-12-05 16:06:40 +0300

    ContextClosedListenerTest ContextStartedListenerTest

--- src/main/java/tgb/btc/rce/config/ContextClosedListener.java
+++ src/main/java/tgb/btc/rce/config/ContextClosedListener.java
@@ -2,12 +2,10 @@ package tgb.btc.rce.config;
 
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
-import org.springframework.lang.Nullable;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import tgb.btc.library.constants.enums.bot.UserRole;
 import tgb.btc.rce.service.INotifyService;
-
-import java.util.Objects;
 
 @Component
 public class ContextClosedListener implements ApplicationListener<ContextClosedEvent> {
@@ -19,9 +17,7 @@ public class ContextClosedListener implements ApplicationListener<ContextClosedE
     }
 
     @Override
-    public void onApplicationEvent(@Nullable ContextClosedEvent event) {
-        if (Objects.nonNull(event)) {
-            notifyService.notifyMessage("⚠️ Рестарт бота.", UserRole.OPERATOR_ACCESS, false);
-        }
+    public void onApplicationEvent(@NonNull ContextClosedEvent event) {
+        notifyService.notifyMessage("⚠️ Рестарт бота.", UserRole.OPERATOR_ACCESS, false);
     }
 }
--- src/test/java/tgb/btc/rce/config/ContextStartedListenerTest.java
+++ src/test/java/tgb/btc/rce/config/ContextStartedListenerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import tgb.btc.library.constants.enums.bot.UserRole;
 import tgb.btc.rce.service.INotifyService;
 
@@ -13,21 +13,20 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
-class ContextClosedListenerTest {
+class ContextStartedListenerTest {
 
     @Mock
     private INotifyService notifyService;
 
     @Mock
-    private ContextClosedEvent event;
+    private ApplicationReadyEvent applicationReadyEvent;
 
     @InjectMocks
-    private ContextClosedListener contextClosedListener;
+    private ContextStartedListener listener;
 
     @Test
-    protected void shouldNotify() {
-        contextClosedListener.onApplicationEvent(event);
-        verify(notifyService, times(1)).notifyMessage("⚠️ Рестарт бота.", UserRole.OPERATOR_ACCESS, false);
+    protected void onApplicationEvent() {
+        listener.onApplicationEvent(applicationReadyEvent);
+        verify(notifyService, times(1)).notifyMessage("\uD83D\uDFE2 Бот запущен.", UserRole.OPERATOR_ACCESS, false);
     }
-
 }
