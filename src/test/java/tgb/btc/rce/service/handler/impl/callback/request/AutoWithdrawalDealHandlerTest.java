72a610af3121346169b79e4baf132564722dfbdd
barilo <e.barilo@isida.by>
2024-12-19 18:44:23 +0300

    AutoWithdrawalDealHandlerTest

--- src/test/java/tgb/btc/rce/service/handler/impl/callback/request/AutoWithdrawalDealHandlerTest.java
+++ src/test/java/tgb/btc/rce/service/handler/impl/callback/request/AutoWithdrawalDealHandlerTest.java
@@ -152,6 +152,60 @@ class AutoWithdrawalDealHandlerTest {
                eq(InlineButton.builder().text("Отмена").data(CallbackQueryData.INLINE_DELETE.name()).build()));
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "BITCOIN, 0.001, btc213mk12424n124j142k12n4",
+            "LITECOIN, 2.51, ltc2qwe1sgd6j8yuj1jfg6fdg8",
+            "MONERO, 0.2, xmr5hfd4hf6h7ht6trh8h4er6qwe4",
+    })
+    @DisplayName("Должен правильно сформировать сообщение запроса подтверждения на вывод.")
+    void shouldBuildMessage(String cryptoCurrencyName, String amount, String wallet) {
+        CallbackQuery callbackQuery = new CallbackQuery();
+        Message message = new Message();
+        User user = new User();
+
+        String data = "data";
+        Integer messageId = 50000;
+        Long chatId = 123456789L;
+        int callbackQueryId = 24005;
+        user.setId(chatId);
+        message.setMessageId(messageId);
+        callbackQuery.setData(data);
+        callbackQuery.setFrom(user);
+        callbackQuery.setMessage(message);
+        callbackQuery.setId(Integer.toString(callbackQueryId));
+
+        Long dealPid = 1000L;
+        CryptoCurrency cryptoCurrency = CryptoCurrency.valueOf(cryptoCurrencyName);
+
+        Deal deal = new Deal();
+        deal.setPid(dealPid);
+        deal.setCryptoAmount(new BigDecimal(amount));
+        deal.setCryptoCurrency(cryptoCurrency);
+        deal.setWallet(wallet);
+
+        Message deleteMessage = new Message();
+        Integer deleteMessageId = 24440;
+        deleteMessage.setMessageId(deleteMessageId);
+
+        String newData = "newData";
+
+        when(callbackDataService.getLongArgument(data, 1)).thenReturn(dealPid);
+        when(readDealService.findByPid(dealPid)).thenReturn(deal);
+        when(responseSender.sendMessage(chatId, "Получение баланса.")).thenReturn(Optional.of(deleteMessage));
+        when(cryptoWithdrawalService.getBalance(cryptoCurrency)).thenReturn(new BigDecimal("500"));
+        when(callbackDataService.buildData(CallbackQueryData.CONFIRM_AUTO_WITHDRAWAL_DEAL, dealPid, messageId)).thenReturn(newData);
+
+        autoWithdrawalDealHandler.handle(callbackQuery);
+
+        verify(responseSender).sendMessage(eq(chatId), eq("Вы собираетесь отправить " + amount
+                        + " " + cryptoCurrency.getShortName() + " на адрес <code>" + wallet + "</code>. Продолжить?"),
+                eq(InlineButton.builder()
+                        .text("Продолжить")
+                        .data(newData).build()),
+                eq(InlineButton.builder().text("Отмена").data(CallbackQueryData.INLINE_DELETE.name()).build()));
+    }
+
     @Test
     void getCallbackQueryData() {
         assertEquals(CallbackQueryData.AUTO_WITHDRAWAL_DEAL, autoWithdrawalDealHandler.getCallbackQueryData());
